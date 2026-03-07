0178017893f9e6c8bf797745a2dffbdbab4c47b0
PRERNA SINHA <prernasinha553@gmail.com>
2026-03-07 19:09:53 +0530

    Add files via upload

---
+++ 16SpringCoreDependencyInjection2/src/main/java/com/prerna/Student.java
@@ -0,0 +1,24 @@
+package com.prerna;
+
+public class Student {
+	
+	private String name;
+	private int rollno;
+	private Address adress;
+	
+	
+	public Student(String name, int rollno, Address adress) {
+		super();
+		this.name = name;
+		this.rollno = rollno;
+		this.adress = adress;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", rollno=" + rollno + "]";
+	}
+	
+
+}
